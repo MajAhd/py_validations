f5def7933fa0ea9930c4fff268e15f98201350ce
Majid Ahmaditabar <mjd.ahd.tbr@gmail.com>
2021-12-09 02:06:12 +0330

    Validation : Nullable Added

---
+++ tests/test_nullable.py
@@ -0,0 +1,36 @@
+import unittest
+import pyvalidation as PyValidation
+
+
+class TestNullable(unittest.TestCase):
+
+    def test_pyvalidation_nullable_passed(self):
+        data = {
+            "name_1": "",
+            "name_2": "John",
+            "name_3": None,
+        }
+        rules = {
+            "name_1": ["nullable", "alpha"],
+            "name_2": ["nullable", "alpha"],
+            "name_3": ["nullable", "alpha"],
+        }
+        validate = PyValidation.PyValidation(data, rules).make()
+        self.assertEqual(validate, {})
+
+    def test_pyvalidation_nullable_failed(self):
+        data = {
+            "name_1": "john 1",
+            "number_2": "a2",
+        }
+        rules = {
+            "name_1": ["nullable", "alpha"],
+            "number_2": ["nullable", "numeric"],
+        }
+        validate = PyValidation.PyValidation(data, rules).make()
+        self.assertEqual(validate, {'name_1': ['The name_1 may only contain letters.'],
+                                    'number_2': ['The number_2 must be a number.']})
+
+
+if __name__ == '__main__':
+    unittest.main()
